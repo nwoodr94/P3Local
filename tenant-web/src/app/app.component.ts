c25da87a9ffc003686ed61723190317d1a959b0a
Nathan Woodruff <nwoodr94@gmail.com>
2019-07-09 09:53:16 -0500

    merger

--- tenant-web/src/app/app.component.ts
+++ tenant-web/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,13 +6,9 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
-  constructor(private router: Router){
-  }
-
-  // goToPage(pageName:string){
-  //   this.router.navigate([`${pageName}`]);
-  // }
-
   title = 'tenant-web';
+
+  constructor(){
+    console.log("Fred Brume");
+  }
 }
